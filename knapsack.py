3e7716e792d994c0547b6899dc65ac9d35460fa3
Tiago Barros <tiagobarros99@ua.pt>
2025-03-11 14:40:25 +0000

    chore: Added type hints

--- knapsack.py
+++ knapsack.py
@@ -5,7 +5,7 @@ import yfinance as yf
 from typing import Any
 
 
-def calculate_allocation(prices, budget, window=10):
+def calculate_allocation(prices: dict[str, float], budget: int, window=10):
     """
     prices = current ticker prices
     budget = total amount of money to be invested
@@ -14,21 +14,24 @@ def calculate_allocation(prices, budget, window=10):
     """
 
     # Get the max quantities that can be bough of each ETF given the budget.
-    max_quantities = {}
+    max_quantities: dict[str, int] = {}
     for etf, cost in prices.items():
         max_quantities[etf] = int(budget // cost)
 
     # Create a list of range objects, one for each ETF
-    quantity_ranges = {}
+    quantity_ranges: dict[str, range] = {}
     for etf, mq in max_quantities.items():
         quantity_ranges[etf] = range(mq + 1)
 
     # Generate all possible combinations of the varying
     # quantities of the ETFs calculated in the previous steps
-    products = itertools.product(*quantity_ranges.values())
+    # Ex: [(2, 0, 6), (2, 0, 7), (2, 1, 0), (2, 1, 1),,...]
+    # The order of the tuples is the same as the dictionary keys, since python
+    # maintains the read order on every iteration. see: `product_to_combination`
+    products = list(itertools.product(*quantity_ranges.values()))
     all_combinations = product_to_combination(prices, products)
 
-    valid_combinations = []
+    valid_combinations: list[dict[str, int]] = []
     for comb in all_combinations:
         price = calculate_buy_price(prices, comb)
 
@@ -39,34 +42,38 @@ def calculate_allocation(prices, budget, window=10):
     return valid_combinations
 
 
-def product_to_combination(prices, products):
+def product_to_combination(prices: dict[str, float], products: list[tuple[int, ...]]):
     """
     For every product with the various quantities of the ETFs,
     associate said quantities to the corresponding ETF.
     This hinges on the fact that every read of a python dictionary
-    is the same, i.e., the order of etfs is the same.
+    is the same on every read operation, i.e., the order of the keys of the dictionary in this function
+    will be in the same order from when we calculated the various quantities previously, allowing us
+    to map the quantities to the keys.
+    Ex: `{'VUAA': 2, 'VWCE': 0, 'QDVE': 2}`
     """
-    combinations = []
+
+    combinations: list[dict[str, int]] = []
     for product in products:
         combinations.append(dict(zip(prices.keys(), product)))
 
     return combinations
 
 
-def calculate_buy_price(prices, combination):
+def calculate_buy_price(prices: dict[str, float], combination: dict[str, int]):
     """Calculate the total amount that
     would be spent of the budget if this
     combination were to take place
     """
 
-    total = 0
+    total = 0.0
     for etf, quantity in combination.items():
         total += quantity * prices[etf]
 
     return total
 
 
-def calculate_commission(combination):
+def calculate_commission(combination: dict[str, int]):
     """
     In IBKR, for each transaction, the normal commission
     is of 1,25€
@@ -78,14 +85,16 @@ def calculate_commission(combination):
     return transactions * 1.25
 
 
-def calculate_new_balance(prices, allocation, combination):
+def calculate_new_balance(
+    prices: dict[str, float], allocation: dict[str, int], combination: dict[str, int]
+):
     """
     Calculate portfolio balance with the new ETF ammount.
     This will give the weight for each ETF if a given combination choice is taken.
     """
     # Calculate the total amount of value this allocation would have if this combinations
     # were to be bought so that we can calculate the weight after.
-    total_money = 0
+    total_money = 0.0
     for etf, quantity in allocation.items():
         # Money quantity is a special case
         if etf == "MONEY":
@@ -93,7 +102,7 @@ def calculate_new_balance(prices, allocation, combination):
         else:
             total_money += (quantity + combination[etf]) * prices[etf]
 
-    new_weights = {}
+    new_weights: dict[str, float] = {}
     for etf, quantity in allocation.items():
         # Money quantity is a special case
         if etf == "MONEY":
@@ -106,12 +115,12 @@ def calculate_new_balance(prices, allocation, combination):
     return new_weights
 
 
-def calculate_current_balance(prices, allocation):
+def calculate_current_balance(prices: dict[str, float], allocation: dict[str, int]):
     """
     Calculate the current portfolio balance.
     """
 
-    total_money = 0
+    total_money = 0.0
     for etf, quantity in allocation.items():
         # Money quantity is a special case
         if etf == "MONEY":
@@ -119,7 +128,7 @@ def calculate_current_balance(prices, allocation):
         else:
             total_money += quantity * prices[etf]
 
-    weights = {}
+    weights: dict[str, float] = {}
     for etf, quantity in allocation.items():
         # Money quantity is a special case
         if etf == "MONEY":
@@ -130,8 +139,8 @@ def calculate_current_balance(prices, allocation):
     return weights, total_money
 
 
-def get_ticker_prices(tickers):
-    prices = {}
+def get_ticker_prices(tickers: dict[str, str]):
+    prices: dict[str, float] = {}
     for etf, ticker in tickers.items():
         # get the current bid price
         prices[etf] = yf.Ticker(ticker).info["bid"]
@@ -139,12 +148,26 @@ def get_ticker_prices(tickers):
     return prices
 
 
+def delta(new_weight: float, current_weight: float):
+    difference = round(abs(new_weight - current_weight), 2)
+
+    if new_weight > current_weight:
+        return f"+{difference}"
+    else:
+        return f"-{difference}"
+
+
 def load_info() -> dict[str, Any]:
     with open("info.toml", "rb") as f:
         return tomllib.load(f)
 
 
-def print_combinations(prices, quantity, combinations, balance):
+def print_combinations(
+    prices: dict[str, float],
+    quantity: dict[str, int],
+    combinations: list[dict[str, int]],
+    balance: dict[str, float],
+):
     """
     Pretty print the options and possible buy combinations given the budget.
     The `balance` argument is the current balance so we can calculate the delta.
@@ -168,15 +191,6 @@ def print_combinations(prices, quantity, combinations, balance):
                 ]
             ),
         )
-
-
-def delta(new_weight, current_weight):
-    difference = round(abs(new_weight - current_weight), 2)
-
-    if new_weight > current_weight:
-        return f"+{difference}"
-    else:
-        return f"-{difference}"
 
 
 if __name__ == "__main__":
